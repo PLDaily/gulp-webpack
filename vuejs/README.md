387eb5bf14582f7eb5c06ba818cdf9a8727115d1
PLDaily <pcd12321@gmail.com>
2016-11-16 13:02:57 +0800

    修改

--- vuejs/README.md
+++ vuejs/README.md
@@ -1,1 +1,14 @@
 # gulp+webpack在vuejs中的搭建
+
+1.安装依赖
+```git
+$ npm install
+```
+2.本地调试
+```git
+$ gulp dev
+```
+3.发布
+```git
+$ gulp
+```
--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -1,25 +1,8 @@
 import Vue from 'vue'
 import App from './components/App.vue'
-import Home from './components/Home.vue'
-import Users from './components/Users.vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-Vue.use(VueResource)
-Vue.use(VueRouter)
 
-var router = new VueRouter()
-
-router.map({
-  '/home': {
-    component: Home
-  },
-  '/users': {
-    component: Users
-  },
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
 })
-
-router.redirect({
-  '*': '/home'
-})
-
-router.start(App, '#app')
