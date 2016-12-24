6bb3b61cd64ee4bbfc2c323035d767617d0553bb
PLDaily <pcd12321@gmail.com>
2016-12-24 13:51:38 +0800

    添加gulp-clean

--- vuejs/gulpfile.js
+++ vuejs/gulpfile.js
@@ -8,8 +8,10 @@ var webpack = require('webpack-stream');
 var connect = require('gulp-connect');
 var uglify = require('gulp-uglify');
 var md5 = require('gulp-md5-plus');
+var clean = require('gulp-clean');
 
-gulp.task('webpack', function() {
+
+gulp.task('webpack',['build.img'], function() {
   return gulp.src('./src/main.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('./dist/js'))
@@ -18,31 +20,41 @@ gulp.task('webpack', function() {
 })
 
 gulp.task('watch', function (done) {
-    gulp.watch('src/**/*', ['webpack','build.index'])
+    return gulp.watch('src/**/*', ['webpack'])
         .on('end', done);
 });
 
-gulp.task('script',['webpack'], function() {
+gulp.task('script', ['webpack'], function() {
   return gulp.src('./dist/js/*.js')
     .pipe(uglify())
     .pipe(md5(10, './dist/*.html'))
     .pipe(gulp.dest('./dist/js'))
 })
 
-gulp.task('webserver', function() {
+gulp.task('webserver', ['build.index'], function() {
   connect.server({
     livereload: true,
     root: './dist'
   });
 });
 
-gulp.task('build.index', function(){
+gulp.task('build.img', ['build.index'], function() {
+  return gulp.src('./src/img/**')
+  .pipe(gulp.dest('./dist/img'));
+})
+
+gulp.task('build.index', ['clean'], function(){
   return gulp.src('./index.html')
     .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('clean', function () {
+    return gulp.src('./dist')
+        .pipe(clean());
+});
+
 //发布
-gulp.task('default', ['webserver', 'webpack', 'build.index', 'script']);
+gulp.task('default', ['clean', 'webpack', 'build.index', 'build.img', 'script', 'webserver']);
 
 //测试
-gulp.task('dev', ['webserver', 'webpack', 'build.index', 'watch']);
+gulp.task('dev', ['clean', 'webpack', 'build.index', 'build.img', 'watch', 'webserver']);
